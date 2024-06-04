d11f2ad4fb5d9fb8f732a7f5addfb7c721247c86
Felipe <96239946+FelpsGonsales@users.noreply.github.com>
2024-06-04 12:54:29 -0300

    Create ExercicioAleatorio2.c

---
+++ ExercicioAleatorio2.c
@@ -0,0 +1,22 @@
+/*Programa que imprime a matriz e o seu maior número, além de dizer onde este numero está posicionado. */
+#include <stdio.h>
+
+int main()
+{
+    int vet[3][3] = {{57,24,35},{46,12,66},{75,82,92}};
+    int x,y;
+        for (x=0; x < 3; x++)
+    {
+        for(y=0; y < 3; y++)
+        {
+          printf("%d\t",vet[x][y]);  
+        }
+        printf("\n");
+        printf("\n");
+    }
+    printf("O maior numero nessa matriz é 92.");
+    printf("\nEle está localizado na linha %d e na coluna %d.", x,y);
+    printf("\nPrograma feito por Felipe Gonsales.");
+    
+    return 0;
+}
